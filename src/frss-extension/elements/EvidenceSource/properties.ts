709ba832fe5a24f1f8fc68861f8014504a6e1e1a
Tomáš Sedláček <dev.sedlacektom@gmail.com>
2022-11-28 15:34:14 +0100

    chore: ESLint

--- src/frss-extension/elements/EvidenceSource/properties.ts
+++ src/frss-extension/elements/EvidenceSource/properties.ts
@@ -1,6 +1,8 @@
 // import the prefix
 import { bpmn4frssPrefix } from '../../common';
-import { RenderableElementWithIconProps } from '../../types/properties/properties';
+import {
+  RenderableElementWithIconProps,
+} from '../../types/properties/properties';
 import icon
   from './assets/evidence-source.png';
 
