5d9f6114178ad20c584efe52e9785257bdcc42e7
Tomáš Sedláček <dev.sedlacektom@gmail.com>
2023-04-17 11:19:36 +0200

    fix: Bringing the EvidenceSource rules back

--- src/frss-extension/elements/EvidenceSource/rules.ts
+++ src/frss-extension/elements/EvidenceSource/rules.ts
@@ -1,61 +1,61 @@
-// // @ts-ignore
-// import { is, isAny } from 'bpmn-js/lib/util/ModelUtil';
+// @ts-ignore
+import { is, isAny } from 'bpmn-js/lib/util/ModelUtil';
 
-// import { ElementRules } from '../../types/rules';
-// import ProducesProperties from '../Produces/properties';
-// import properties from './properties';
+import { ElementRules } from '../../types/rules';
+import ProducesProperties from '../Produces/properties';
+import properties from './properties';
 
-// const evidenceSourceIdentifier = properties.identifier;
+const evidenceSourceIdentifier = properties.identifier;
 
-// const checkAttachmentOrCreation = (shape: any, _target: any): boolean => (
-//   is(shape, evidenceSourceIdentifier)
-// );
+const checkAttachmentOrCreation = (shape: any, _target: any): boolean => (
+  is(shape, evidenceSourceIdentifier)
+);
 
-// // what elements can we attach the potential evidence source to
-// const attachableTo: string[] = [
-//   'bpmn:Task', 'bpmn:Event', 'bpmn:DataStoreReference',
-// ];
+// what elements can we attach the potential evidence source to
+const attachableTo: string[] = [
+  'bpmn:Task', 'bpmn:Event', 'bpmn:DataStoreReference',
+];
 
-// const rules: ElementRules = {
-//   attachmentRule: (source, target) => {
-//     // evidence source can attach to tasks, events, and data store references
-//     if (is(source, evidenceSourceIdentifier)) {
-//       if (isAny(target, attachableTo)) {
-//         return 'attach';
-//       }
-//       return false;
-//     }
-//   },
-//   connectionRule: (source, target) => {
-//     // handle should be reversed when the target is the evidence source identifier
-//     if (is(target, evidenceSourceIdentifier)) return false;
+const rules: ElementRules = {
+  attachmentRule: (source, target) => {
+    // evidence source can attach to tasks, events, and data store references
+    if (is(source, evidenceSourceIdentifier)) {
+      if (isAny(target, attachableTo)) {
+        return 'attach';
+      }
+      return false;
+    }
+  },
+  connectionRule: (source, target) => {
+    // handle should be reversed when the target is the evidence source identifier
+    if (is(target, evidenceSourceIdentifier)) return false;
 
-//     // then if the source is not the evidence source identifier
-//     if (!is(source, evidenceSourceIdentifier)) return;
+    // then if the source is not the evidence source identifier
+    if (!is(source, evidenceSourceIdentifier)) return;
 
-//     // if the target is the DataObjectReference which is marked as the
-//     // potential evidence
-//     if (
-//       is(target, 'bpmn:DataObjectReference')
-//       && target.businessObject?.dataObjectRef?.isPotentialEvidence
-//     ) {
-//       return {
-//         type: ProducesProperties.identifier,
-//       };
-//     }
+    // if the target is the DataObjectReference which is marked as the
+    // potential evidence
+    if (
+      is(target, 'bpmn:DataObjectReference')
+      && target.businessObject?.dataObjectRef?.isPotentialEvidence
+    ) {
+      return {
+        type: ProducesProperties.identifier,
+      };
+    }
 
-//     // otherwise it should not have occurred
-//     return false;
-//   },
-//   creationRule: (source, target) => (
-//     is(source, evidenceSourceIdentifier)
-//       && isAny(target, attachableTo)
-//   ),
-//   shouldCheckAttachment: checkAttachmentOrCreation,
-//   shouldCheckCreation: checkAttachmentOrCreation,
-//   shouldCheckConnection: (source, target) => (
-//     is(source, evidenceSourceIdentifier) || is(target, evidenceSourceIdentifier)
-//   ),
-// };
+    // otherwise it should not have occurred
+    return false;
+  },
+  creationRule: (source, target) => (
+    is(source, evidenceSourceIdentifier)
+      && isAny(target, attachableTo)
+  ),
+  shouldCheckAttachment: checkAttachmentOrCreation,
+  shouldCheckCreation: checkAttachmentOrCreation,
+  shouldCheckConnection: (source, target) => (
+    is(source, evidenceSourceIdentifier) || is(target, evidenceSourceIdentifier)
+  ),
+};
 
-// export default rules;
+export default rules;
