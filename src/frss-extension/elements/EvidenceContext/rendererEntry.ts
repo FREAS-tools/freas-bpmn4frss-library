32c41ca545478aee6d75b739dbd14ac31dfdc09c
Tomáš Sedláček <dev.sedlacektom@gmail.com>
2023-05-02 12:45:23 +0200

    fix: Fixing cooperativeness visuals

--- src/frss-extension/elements/EvidenceContext/rendererEntry.ts
+++ src/frss-extension/elements/EvidenceContext/rendererEntry.ts
@@ -17,44 +17,52 @@ import {
 } from '../../types/renderer';
 import type { FrssCooperativenessType } from '../Cooperativeness/enumeration';
 
+type CooperativenessPoolElement = {
+  businessObject: {
+    cooperativeness: FrssCooperativenessType | undefined,
+  },
+};
+
 const renderFunction: RenderFunction = (
   { parentNode, element, bpmnRenderer },
 ) => {
+  const cooperativenessElement: CooperativenessPoolElement = element;
+
   const evidenceContext = bpmnRenderer.handlers['bpmn:Participant'](
     parentNode,
-    element,
+    cooperativenessElement,
   );
 
-  const getColor = (elem: {
-    businessObject: {
-      cooperativeness: FrssCooperativenessType
-    },
-  }) => {
-    switch (elem?.businessObject?.cooperativeness) {
-      case 'Cooperative': return '#004200';
-      case 'SemiCooperative': return '#b8ab00';
-      case 'NonCooperative': return '#754c00';
-      default: return '#fffff';
+  const getAttributes = () => {
+    switch (cooperativenessElement.businessObject.cooperativeness) {
+      case 'Cooperative': return {
+        stroke: '#004200',
+        fill: '#40ff40',
+        'fill-opacity': 0.10,
+      };
+      case 'SemiCooperative': return {
+        stroke: '#776f00',
+        fill: '#fff01e',
+        'fill-opacity': 0.10,
+      };
+      default: return {};
     }
   };
 
-  console.log(getColor(element));
-
-  const attributes = {
-    stroke: getColor(element),
-  };
-
-  attributesSvg(evidenceContext, attributes);
-
+  attributesSvg(evidenceContext, getAttributes());
   appendSvg(parentNode, evidenceContext);
-  console.log(evidenceContext);
   return evidenceContext;
 };
 
 const rendererEntry: RendererEntry = {
   renderFunction,
-  shouldRender: (element) => is(element, 'bpmn:Participant')
-      && element?.businessObject?.cooperativeness !== undefined,
+  shouldRender: (
+    element: CooperativenessPoolElement,
+  ) => is(element, 'bpmn:Participant')
+       && (
+         element.businessObject.cooperativeness === 'Cooperative'
+        || element.businessObject.cooperativeness === 'SemiCooperative'
+       ),
   type: ElementRenderType.Shape,
 };
 
